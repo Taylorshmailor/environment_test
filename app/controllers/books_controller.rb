b829b280208b9e8864de642bcabfcbeb4af8ef51
root <root@localhost>
2022-02-04 03:33:51 +0000

    Fixing delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -49,12 +49,17 @@ class BooksController < ApplicationController
 
   # DELETE /books/1 or /books/1.json
   def destroy
+    @book = Book.find(params[:id])
     @book.destroy
 
     respond_to do |format|
       format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
   end
 
   private
